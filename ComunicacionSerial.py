0636b70cac02fda1618b314df39bcf7fa3f9c501
jorgecedeno <jorgecedeno26@gmail.com>
2018-03-30 21:46:55 -0400

    Add files via upload
    Modificacion en el Código ComunicacionSerial donde se hace que la funcion leerpuerto haga un return

--- ComunicacionSerial.py
+++ ComunicacionSerial.py
@@ -46,17 +46,27 @@ xdata2 = []
 ydata1 = []
 ydata2 = []
 
+bandera = 0
+
 def leerPuerto():
     mensaje = port.read(1 + 2 * cantidad_canales)
     digital1 = (mensaje[1] & 64) >> 6  # entrada digital 1
     digital2 = (mensaje[1] & 32) >> 5  # entrada digital 2
+    digital2 = 1 #Mientras tanto porque no tenemos el Sensor de Ultrasonido
+    if(digital1==0 and digital2==1):
+        valor = 1
+        comapp.deteccion(valor)
 
-    print(digital1," ",digital2)
+    #print(digital1," ",digital2)
     for canal in range(0,2):
         analogico[canal] = (((mensaje[1+2*canal] & 31) << 7) + mensaje[2+2*canal])/2**12
-        #analogico[canal]=np.vstack((analogico[canal],valor))
-    comapp.referencia(50*analogico[1]) #Se manda el valor de la Temperatura a la funcion referencia de comunicacionapp.py
+    sismo = MAX_ACELEROMETRO * analogico[0]
+    temperatura = MAX_TEMPERATURA * analogico[1]
+    print(sismo, " ", temperatura)
+
+    comapp.referencia(temperatura) #Se manda el valor de la Temperatura a la funcion referencia de comunicacionapp.py
     threading.Timer(1/10000.,leerPuerto).start()
+    return sismo
 
 def escribirPuerto():
     while 1:
@@ -71,14 +81,14 @@ def next():
         yield i
 def update(i):
     y = MAX_ACELEROMETRO * analogico[0]
-    if(y<=1.4):
-        comapp.sismo(y)
+    # if(y<=1.3): #Condicional para que se procese la medida del sismo
+    #     comapp.sismo(y)
     ydata1.append(y)
     xdata1.append(i)
     lineas[0].set_data(xdata1, ydata1)
 
     y = MAX_TEMPERATURA * analogico[1]
-    comapp.referencia(y) #Se manda el valor de la Temperatura a la funcion referencia de comunicacionapp.py
+    # comapp.referencia(y) #Se manda el valor de la Temperatura a la funcion referencia de comunicacionapp.py
     #Graficación de la señal de Temperatura:
     ydata2.append(y)
     xdata2.append(i)
@@ -94,9 +104,25 @@ def update(i):
 if __name__ == '__main__':
     a = animation.FuncAnimation(fig, update, next, blit = True, interval = 60,
                                repeat = False)
-    leerPuerto()
+    sismo = leerPuerto()
+
+    if(sismo <= 1.4 and bandera == 0):  # Condicional para que se procese la medida del sismo. bandera == 0 significa que no habia un sismo antes
+        valores_acelerometro = np.zeros((0, 1))  # Se inicializa vacio el arreglo de los valores_acelerometro
+        tiempo_inicial = time.time()  # Se establece el tiempo inicial para empezar a contar en segundos
+        valores_acelerometro = np.vstack([valores_acelerometro,sismo])
+        bandera = 1
+
+    if(sismo <= 1.4 and bandera == 1): #Continúa el sismo (bandera = 1). Por lo tanto, se sigue almacenando el valor del sismo
+        valores_acelerometro = np.vstack([valores_acelerometro,sismo])
+
+    if(sismo > 1.4 and bandera == 1): #Paró el sismo. Se hace bandera = 0
+        tiempo_final = time.time() - tiempo_inicial  # Se establece el tiempo final restando el tiempo actual menos el tiempo inicial (segundos)
+        tiempo_final_entero = int(tiempo_final)
+        amplitud_sismo = valores_acelerometro.min()
+        bandera = 0
+        comapp.sismo(amplitud_sismo,tiempo_final_entero)  # Se manda el valor del Sismo a la función sismo de comunicacionapp.py
+
     escribirPuerto()
     plt.show()
-
     _, y =np.asarray(linea1.get_data())
     ans.analisis(y)
--- comunicacionapp.py
+++ comunicacionapp.py
@@ -1,8 +1,11 @@
+#comunicacionapp
+
 import firebase_admin
 import time
 from firebase_admin import credentials
 from firebase_admin import db
 import math
+import numpy as np
 
 # Fetch the service account key JSON file contents
 cred = credentials.Certificate('domoticaec3882-firebase-adminsdk-rye8h-493394c76b.json')
@@ -18,16 +21,15 @@ def referencia(temperatura):
     temperaturaRef.set(temperatura) #Se manda a Firebase el valor de la Temperatura
     print(temperaturaRef.get())
 
-def sismo(acelerometro):
-    tiempo_inicial = time.time() #Se establece el tiempo inicial para empezar a contar en segundos
-    amplitud_inicial = 1.5 - acelerometro #Como el acelerometro siempre muestra 1.5 en la salida y baja dicho valor cuando se mueve. Entonces la amplitud la calculamos de esa manera
-    while(acelerometro <= 1.45): #Se establece un ciclo infinito hasta que el sismo pase
-       if(acelerometro <= amplitud_inicial):
-            amplitud_maxima = 1.5 - acelerometro
-    tiempo_final = time.time() - tiempo_inicial  # Se establece el tiempo final restando el tiempo actual menos el tiempo inicial (segundos)
-    tiempo_final_entero = int(tiempo_final)
-    magnitud = math.log((((amplitud_maxima*1000) * (tiempo_final_entero ** 3)) / 1.62),10)  # Se hace el cálculo en la escala de Richter
-    magnitud = int(magnitud)
+def deteccion(valor):
+    alarmaRef = db.reference('Alarma')
+    alarmaRef.set(valor) #Se manda la alarma a Firebase
+
+
+def sismo(acelerometro,tiempo):
+    amplitud_maxima = 1.43 - acelerometro #Como el acelerometro siempre muestra 1.43 en la salida y baja dicho valor cuando se mueve. Entonces la amplitud la calculamos de esa manera
+    magnitud = math.log((((amplitud_maxima*1000) * (tiempo ** 3)) / 1.62),10)  # Se hace el cálculo en la escala de Richter
+    magnitud = round(magnitud,1)
     magnitudRef = db.reference('Sismo')
     magnitudRef.set(magnitud)  # Se manda a Firebase el valor del Sismo
     print(magnitudRef.get())
